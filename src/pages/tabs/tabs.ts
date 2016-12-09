ba78eee0387d9fd770044346df3d38196c1dcb69
Marco Berardini <mberardini@evolution-man.com>
2016-12-09 14:41:01 -0800

    [client] Added WeatherComponent to tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
+import {WeatherComponent} from "../weather/weather";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -11,6 +12,7 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = HomePage;
+  weatherRoot: any = WeatherComponent;
   tab2Root: any = AboutPage;
   tab3Root: any = ContactPage;
 
--- src/pages/weather/weather.ts
+++ src/pages/weather/weather.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core'
 
 
 @Component({
-  selector: 'my-weather'
+  selector: 'my-weather',
   template: `
       <ion-header>
         <ion-navbar>
@@ -14,7 +14,7 @@ import { Component } from '@angular/core'
       </ion-header>
       
       <ion-content padding>
-      
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus similique, voluptatibus! A at, atque deleniti deserunt distinctio eius eos excepturi fugiat ipsa perspiciatis placeat ratione similique sunt totam ut voluptatum.
       </ion-content>
 `
 })
