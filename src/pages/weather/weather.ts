5940e7537e08f6b24df95774149715d7e3512a53
Marco Berardini <mberardini@evolution-man.com>
2016-12-09 14:37:52 -0800

    [client] Scaffolded Ionic app and added WeatherComponent.

---
+++ src/pages/weather/weather.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core'
+
+
+
+@Component({
+  selector: 'my-weather'
+  template: `
+      <ion-header>
+        <ion-navbar>
+          <ion-title>
+            Weather
+          </ion-title>
+        </ion-navbar>
+      </ion-header>
+      
+      <ion-content padding>
+      
+      </ion-content>
+`
+})
+
+export class WeatherComponent {
+
+
+
+}
